b5d8cd2e1639f16aebc986c2f8feb0657ed0fe8e
yogaadityaw <yogaaawi12345@gmail.com>
2023-09-26 19:55:58 +0700

    Delete unused file Co-authored-by: Rizky Ifku <sensze@users.noreply.github.com>

---
+++ middleware/Validation.php
@@ -0,0 +1,12 @@
+<?php
+//class Validation
+//{
+  //  public static function isValidInput($barangbaik, $barangrusak, $jumlah)
+   // {
+     //   if ($barangbaik == "" || $barangrusak == "" || $jumlah == "") {
+       //     return false;
+        //} else {
+          //  return true;
+        //}
+    //}
+//} 
