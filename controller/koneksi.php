34ea993da088331a85368e96ad8d2f388300eed6
yogaadityaw <yogaaawi12345@gmail.com>
2023-09-28 20:56:55 +0700

    add .env

--- controller/koneksi.php
+++ controller/koneksi.php
@@ -1,13 +1,17 @@
 <?php
-    $hostname = "localhost";
-    $user = "root";
-    $password = "";
-    $database = "gudang_harkan";
-    // ini_set('max_execution_time', 500); // Ganti 300 dengan jumlah detik yang Anda inginkan
+require_once __DIR__ . '/../vendor/autoload.php';
 
-    $conn = mysqli_connect($hostname, $user, $password, $database);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-    if (!$conn) {
-        die("Koneksi database gagal: " . mysqli_connect_error());
-    }
-?>
+$hostname = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$database = "gudang_harkan";
+// ini_set('max_execution_time', 500); // Ganti 300 dengan jumlah detik yang Anda inginkan
+
+$conn = mysqli_connect($hostname, $user, $password, $database);
+
+if (!$conn) {
+    die("Koneksi database gagal: " . mysqli_connect_error());
+}
